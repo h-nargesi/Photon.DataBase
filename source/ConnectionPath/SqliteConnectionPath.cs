cc83fef6f11426c3f7e0a6e74600b889b77809e2
Hamed Nargesi <hamed.nargesi.jar@gmail.com>
2021-05-28 16:55:32 +0430

    the version added to sqlite connection path

--- source/ConnectionPath/SqliteConnectionPath.cs
+++ source/ConnectionPath/SqliteConnectionPath.cs
@@ -6,25 +6,26 @@ namespace Photon.Database
 {
     public class SqliteConnectionPath : IConnectionPath
     {
-        public SqliteConnectionPath(string path) : this(path, null) {}
-        
-        public SqliteConnectionPath(string path, string password) {
-            if (String.IsNullOrEmpty(path))
+        public SqliteConnectionPath(string path, string version = null, string password = null)
+        {
+            if (String.IsNullOrWhiteSpace(path))
                 throw new InvalidDataException("The path is null or empty.");
             this.path = path;
+            this.version = version;
             this.password = password;
         }
 
-        public readonly string path, password;
-        
+        public readonly string path, version, password;
+
         public IConnectionPath Copy()
         {
-            return new SqliteConnectionPath(path, password);
+            return new SqliteConnectionPath(path, version, password);
         }
 
         public override string ToString()
         {
             string result = $"Data Source={path};";
+            if (version != null) result += $"Version={version};";
             if (password != null) result += $"Password={password};";
             return result;
         }
