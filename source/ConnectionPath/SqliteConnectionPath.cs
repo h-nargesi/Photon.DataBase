2fd914bbdb12ea102510ccd8f33ea794c33fc83d
Rayan <Rayan@photon-ai.ir>
2021-09-02 20:07:32 +0430

    روشن کردن کلید خارجی در مسیر کانکشن

--- source/ConnectionPath/SqliteConnectionPath.cs
+++ source/ConnectionPath/SqliteConnectionPath.cs
@@ -6,16 +6,18 @@ namespace Photon.Database
 {
     public class SqliteConnectionPath : IConnectionPath
     {
-        public SqliteConnectionPath(string path, string version = null, string password = null)
+        public SqliteConnectionPath(string path, string version = null, string password = null, bool? foreign_keys = true)
         {
             if (String.IsNullOrWhiteSpace(path))
                 throw new InvalidDataException("The path is null or empty.");
             this.path = path;
             this.version = version;
             this.password = password;
+            this.foreign_keys = foreign_keys;
         }
 
         public readonly string path, version, password;
+        public readonly bool? foreign_keys;
 
         public IConnectionPath Copy()
         {
@@ -27,6 +29,7 @@ namespace Photon.Database
             string result = $"Data Source={path};";
             if (version != null) result += $"Version={version};";
             if (password != null) result += $"Password={password};";
+            if (foreign_keys != null) result += $"foreign_keys={foreign_keys.ToString().ToLower()}";
             return result;
         }
     }
